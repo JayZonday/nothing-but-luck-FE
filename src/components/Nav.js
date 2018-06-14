56d9ca0dcf8bb4b19afaaef1bff2a35dda22e25f
JMoloughney <github email address>
2018-06-14 14:17:16 -0400

    fixed members button

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -7,7 +7,7 @@ class Nav extends React.Component{
     <div className="navBar">
       <Link to='/login'><button id='Login' >Login into NBL</button></Link>
       <Link to='/profile'><button>Your NBL-Zone</button></Link>
-      <Link to='/members'><button id='members' >NBL Members</button></Link>
+      <Link to='/'><button id='members' >NBL Members</button></Link>
       <Link to='/nfl'><button id='nflButton' >NFL</button></Link>
       <Link to='/mlb'><button id='mlbButton' >MLB</button></Link>
       <Link to='/nba'><button id='nbaButton' >NBA</button></Link>
