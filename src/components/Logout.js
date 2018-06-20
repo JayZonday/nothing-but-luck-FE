f8b5bbe9f3d7feecb83979511d5cfe9d034fa005
JMoloughney <github email address>
2018-06-19 23:09:22 -0400

    logout page styling

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -5,7 +5,7 @@ class Logout extends React.Component{
 
     return (
       <div className="not-found">
-      <p> [You've Logged Out Successfully] Have a lucky day and enjoy sport radio-host Mike Franseca on your way out!</p>
+      <p> <span className="logout-msg">[You've Logged Out Successfully]</span> Have a lucky day and enjoy sport radio-host Mike Franseca on your way out!</p>
       <iframe width="560" height="315" src="https://www.youtube.com/embed/cacZvmYCEbQ" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
       </div>
     )
