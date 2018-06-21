a02da4dd444afa6a9c7f7bab8fd8e8eba8079d83
JMoloughney <github email address>
2018-06-20 22:23:57 -0400

    changed logout video

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -5,8 +5,8 @@ class Logout extends React.Component{
 
     return (
       <div className="not-found">
-      <p> <span className="logout-msg">[You've Logged Out Successfully]</span> Have a lucky day and enjoy sport radio-host Mike Franseca on your way out!</p>
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/cacZvmYCEbQ" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+      <p> <span className="logout-msg">[You've Logged Out Successfully]</span> Have a lucky day and enjoy First Take host Stephen A. Smith's take on Phil Jackson on your way out!</p>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/ScHTTDqigwA" frameBorder="0" allow="autoPlay; encrypted-media" allowFullScreen></iframe>
       </div>
     )
   }
