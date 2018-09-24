a9fd30774b39460b5da8b5ab3dba753327bdf7ba
JMoloughney <github email address>
2018-09-24 11:37:38 -0400

    Title styling change and friendlier logout video

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -6,7 +6,7 @@ class Logout extends React.Component{
     return (
       <div className="not-found">
       <p> <span className="logout-msg">[You've Logged Out Successfully]</span><br /> Have a lucky day and enjoy The Grandfather of Sports Talk Radio, Mike Francesa, field a wonderful question from a curious caller!</p>
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/ADrltOpX9GU" frameborder="0" allow="autoplay; encrypted-media" allowFullscreen></iframe>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/QvJEzpH7CfA" frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen></iframe>
       </div>
     )
   }
